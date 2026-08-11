fc45f23f4770176c0d7b571713fd04052fb9377f
satya2544 <koppolu.satya@gmail.com>
2026-08-11 23:56:00 +0100

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 sG_ID="sg-08aa3e7b6d2e04568"
+ZONE_ID="Z0130922213SF6HRWZW9U"
+DOMAIN_NAME="daws85s.online"
 
 for instance in $@
 do
@@ -11,13 +13,34 @@ do
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
        --output text)
+       RECORD_NAME="$instance.$DOMAIN_NAME"
 
    else
        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
        --query 'Reservations[0].Instances[0].PublicIpAddress' \
        --output text)
+       RECORD_NAME="$DOMAIN_NAME"
    fi
 
 
    echo "$instance: $IP"
+
+   
+ aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
 done
