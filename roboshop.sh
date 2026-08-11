4fb6dbf2299158efea28ec1f66118ede84093fdc
satya2544 <koppolu.satya@gmail.com>
2026-08-12 00:22:23 +0100

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -1,56 +1,31 @@
-```bash
 #!/bin/bash
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SG_ID="sg-08aa3e7b6d2e04568"
+sG_ID="sg-08aa3e7b6d2e04568"
 ZONE_ID="Z0130922213SF6HRWZW9U"
 DOMAIN_NAME="daws85s.online"
 
-for instance in "$@"
+for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type t3.micro \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $sG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-    if [ "$instance" != "frontend" ]; then
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$INSTANCE_ID" \
-            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
-            --output text)
+   if [ $instance != "frontend" ]; then
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+       --query 'Reservations[0].Instances[0].PrivateIpAddress' \
+       --output text)
+       RECORD_NAME="$instance.$DOMAIN_NAME"
 
-        RECORD_NAME="$instance.$DOMAIN_NAME"
+   else
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
+       --query 'Reservations[0].Instances[0].PublicIpAddress' \
+       --output text)
+       RECORD_NAME="$DOMAIN_NAME"
+   fi
 
-    else
-        IP=$(aws ec2 describe-instances \
-            --instance-ids "$INSTANCE_ID" \
-            --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text)
 
-        RECORD_NAME="$DOMAIN_NAME"
-    fi
+   echo "$instance: $IP"
 
-    echo "$instance: $IP"
-
-    aws route53 change-resource-record-sets \
-        --hosted-zone-id "$ZONE_ID" \
-        --change-batch "{
-            \"Comment\": \"Updating record set\",
-            \"Changes\": [{
-                \"Action\": \"UPSERT\",
-                \"ResourceRecordSet\": {
-                    \"Name\": \"$RECORD_NAME\",
-                    \"Type\": \"A\",
-                    \"TTL\": 1,
-                    \"ResourceRecords\": [{
-                        \"Value\": \"$IP\"
-                    }]
-                }
-            }]
-        }"
-
+ aws route53 change-resource-record-sets \
+    --hosted-zone-id "$ZONE_ID" \
+    --change-batch "{ \"Comment\": \"Updating record set\", \"Changes\": [{ \"Action\": \"UPSERT\", \"ResourceRecordSet\": { \"Name\": \"$RECORD_NAME\", \"Type\": \"A\", \"TTL\": 1, \"ResourceRecords\": [{ \"Value\": \"$IP\" }] } }] }"
 done
-```
