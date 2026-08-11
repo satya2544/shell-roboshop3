fe0c7bb22dcf2a683a41fee2f2f6b9ae89eecfee
satya2544 <koppolu.satya@gmail.com>
2026-08-11 23:03:36 +0100

    some message

--- roboshop.sh
+++ roboshop.sh
@@ -5,15 +5,15 @@ sG_ID="sg-08aa3e7b6d2e04568"
 
 for instance in $@
 do
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-08aa3e7b6d2e04568 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+   INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $sG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
    if [ $instance != "frontend"]; then
-       IP=$(aws ec2 describe-instances --instance-ids i-0988d52f179662b31 \
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
        --query 'Reservations[0].Instances[0].PrivateIpAddress' \
        --output text text)
 
    else
-       IP=$(aws ec2 describe-instances --instance-ids i-0988d52f179662b31 \
+       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID \
        --query 'Reservations[0].Instances[0].PublicIpAddress' \
        --output text text)
    fi
