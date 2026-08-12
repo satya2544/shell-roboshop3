a120ad85077be541d7ec80ce263116f6f76b93ab
satya2544 <koppolu.satya@gmail.com>
2026-08-12 14:43:59 +0100

    some message

---
+++ catalogue.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+MONGODB_HOST=mongodb.daws85s.online
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other then 0
+fi
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$2 ... $R is Failure $N" | tee -a $LOG_FILE
+     exit 1
+else
+     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+fi    
+
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20..." 
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+VALIDATE $? "creating system user"
+
+
+mkdir /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading catalogue application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+unzip /tmp/catalogue.zip &>>$LOG_FILE
+VALIDATE $? "unzip catalogue"
+npm install &>>$LOG_FILE
+VALIDATE $? "Install dependencies"
+cp catalogue.service /etc/yum.repos.d/catalogue.service
+VALIDATE $? "copy systemctl dervice"
+systemctl daemon-reload
+systemctl enable catalogue &>>$LOG_FILE
+VALIDATE $? "Enable catalogue"
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copy mongo repo"
+
+dnf install mongodb-mongosh -y &>>$LOG_FILE
+VALIDATE $? "Install mongidb client"
+
+mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+VALIDATE $? "load catalogue products"
+
+systemctl restart catalogue
+VALIDATE $? "Creating app directory"
