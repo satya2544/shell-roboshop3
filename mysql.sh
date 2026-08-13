05bc4c5723c253d282da03bdf866568a9a04a28b
satya2544 <koppolu.satya@gmail.com>
2026-08-13 16:07:07 +0100

    some message

---
+++ mysql.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+START_TIME=$(date +%s)
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other then 0
+fi
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$2 ... $R is Failure $N" | tee -a $LOG_FILE
+     exit 1
+else
+     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+fi    
+
+}
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySql Server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling MySql Server"
+
+systemctl start mysqld  
+VALIDATE $? "Starting MySql Server"
+
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "Setting up Root Password"
+
+
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
