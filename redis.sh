5adb75c4d8c21d564eb2aaed2aaab663eee1e55f
satya2544 <koppolu.satya@gmail.com>
2026-08-13 15:17:35 +0100

    some message

--- redis.sh
+++ redis.sh
@@ -39,7 +39,7 @@ VALIDATE $? "Enabling Redis 7"
 dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing Redis" 
 
-sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'  /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no'  /etc/redis/redis.conf
 VALIDATE $? "Allowing Remote connections to Redis" 
 
 
