76331dfee271a4a727eff52de27e6be58029a705
satya2544 <koppolu.satya@gmail.com>
2026-08-13 23:22:31 +0100

    some message

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -33,12 +33,11 @@ fi
 
 }
 
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 dnf install rabbitmq-server -y &>>$LOG_FILE 
 VALIDATE $? "Installing rabbitmq Server"
-
 systemctl enable rabbitmq-server &>>$LOG_FILE 
 VALIDATE $? "Enabling rabbitmq Server"
-
 systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting rabbitmq Server"
 
