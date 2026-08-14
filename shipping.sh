0daa574e752b3c0e2291ec2c97a3c7adcf53eb24
satya2544 <koppolu.satya@gmail.com>
2026-08-14 06:50:20 +0100

    some message

---
+++ shipping.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws85s.online
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+MYSQL_HOST=mysql.daws85s.online
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other then 0
+fi
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$2 ... $R is Failure $N" | tee -a $LOG_FILE
+     exit 1
+else
+     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+fi    
+
+}
+
+dnf install maven -y &>>$LOG_FILE
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+   VALIDATE $? "creating system user"
+else
+   echo -e "User already exist ... $Y SKIPPING $N"
+fi    
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+if [ $USERID -ne 0 ]; then
+     echo "ERROR:: Please run this script with root privelege"
+     exit 1 # failure is other then 0
+fi
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+     echo -e "$2 ... $R is Failure $N" | tee -a $LOG_FILE
+     exit 1
+else
+     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
+fi    
+
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "Disabling NodeJS"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling NodeJS 20..." 
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Installing NodeJS"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+   VALIDATE $? "creating system user"
+else
+   echo -e "User already exist ... $Y SKIPPING $N"
+fi       
+
+
+mkdir -p /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading shipping application"
+
+cd /app 
+VALIDATE $? "Changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/shipping.zip &>>$LOG_FILE
+VALIDATE $? "unzip shipping"
+
+mvn clean package &>>$LOG_FILE
+mv target/shipping-1.0.jar shipping.jar 
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+systemctl daemon-reload
+
+systemctl enable shipping &>>$LOG_FILE
+
+
+
+dnf install mysql -y &>>$LOG_FILE
+
+mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use mysql' &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+   mysql -h $MYSQL_HOST-uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE 
+   mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+else
+   echo -e "Shipping date is already loaded ... $Y SKIPPING $N"
+fi       
+
+systemctl restart shipping
