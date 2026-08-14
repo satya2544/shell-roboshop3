907242f768b863426a281eac40e627e400c2f91f
satya2544 <koppolu.satya@gmail.com>
2026-08-14 08:19:45 +0100

    some message

--- frontend.sh
+++ frontend.sh
@@ -1,58 +1,22 @@
-#!/bin/bash
-
-USERID=$(id -u)
-
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws85s.online
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-mkdir -p $LOGS_FOLDER
-
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-if [ $USERID -ne 0 ]; then
-     echo "ERROR:: Please run this script with root privelege"
-     exit 1 # failure is other then 0
-fi
-
-
-
-VALIDATE(){
-    if [ $1 -ne 0 ]; then
-     echo -e "$2 ... $R is Failure $N" | tee -a $LOG_FILE
-     exit 1
-else
-     echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
-fi    
-
-}
-
-dnf module disable nginx -y &>>$LOG_FILE
-dnf module enable nginx:1.24 -y &>>$LOG_FILE
 dnf install nginx -y &>>$LOG_FILE
-
 VALIDATE $? "Installing Nginx"
+
 systemctl enable nginx &>>$LOG_FILE
-systemctl start nginx 
-VALIDATE $? "Starting Nginx"
+VALIDATE $? "Enabling Nginx"
 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Downloading Nginx"
+VALIDATE $? "Downloading frontend"
 
+cd /usr/share/nginx/html
+unzip -o /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting frontend"
 
-
-rm -rf /etc/nginx/nginx.conf
 cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copying Nginx"
+VALIDATE $? "Copying Nginx configuration"
 
-systemctl restart nginx 
-VALIDATE $? "Restart Nginx"
+nginx -t &>>$LOG_FILE
+VALIDATE $? "Testing Nginx configuration"
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting Nginx"
